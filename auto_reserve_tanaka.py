cdace8bf0619eacdfe6248c8dc71df2e5b39539c
tik-crtl <2005taiyo@gmail.com>
2025-06-26 22:01:15 +0900

    Update auto_reserve_tanaka.py

--- auto_reserve_tanaka.py
+++ auto_reserve_tanaka.py
@@ -1,3 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)  # ドライバパスは自動認識される
+
+
+
 from datetime import datetime, timedelta
 import time
 
